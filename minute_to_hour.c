d668ea6c0702cf5872d28b7f6f6502475277d167
qtcdrey <andreydelrosario8@gmail.com>
2024-11-15 17:15:01 +0800

    practices only

--- minute_to_hour.c
+++ minute_to_hour.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main() {
+    int minute, hour;
+    printf("Given: ");
+    scanf("%d", &minute);
+    hour = minute/60;
+    minute %= 60;
+    printf("Converted hours: %d hours and %d minute/s", hour, minute);
+}
